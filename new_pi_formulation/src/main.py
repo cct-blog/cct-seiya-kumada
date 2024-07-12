39205a0e18115c9f7e56db5448849f678caea46b
Seiya Kumada <seiya.kumada@cct-inc.co.jp>
2024-07-12 17:32:17 +0900

    少し検証した。

--- new_pi_formulation/src/main.py
+++ new_pi_formulation/src/main.py
@@ -40,7 +40,24 @@ def plot_values(pi_values_0, pi_values_1, pi_values_2, pi_values_3, new_limits, 
     if pi_value:
         plt.axhline(y=np.pi, color="r", linestyle="-", linewidth=2, label="True π")
     # plt.xscale("log")
-    # plt.ylim(3.135, 3.15)
+    plt.ylim(3.135, 3.15)
+    plt.xlabel("Number of terms in the series")
+    plt.ylabel("Value of π")
+    plt.title("Approximation of π using the given series with λ=1 and λ=2")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"/home/kumada/projects/cct-seiya-kumada/new_pi_formulation/images/{file_name}.jpg")
+
+
+def plot_diffs(pi_values_0, pi_values_1, pi_values_2, pi_values_3, new_limits, file_name, pi_value=True):
+    # Plotting the results for both λ=1 and λ=2 with actual π value for reference as a red solid line
+    plt.figure(figsize=(10, 6))
+    plt.plot(new_limits, pi_values_0, "o-", label="Approximated π (λ=0)")
+    plt.plot(new_limits, pi_values_1, "o-", label="Approximated π (λ=1)")
+    plt.plot(new_limits, pi_values_2, "o-", label="Approximated π (λ=2)")
+    plt.plot(new_limits, pi_values_3, "o-", label="Approximated π (λ=3)")
+    # plt.xscale("log")
+    plt.ylim(-0.0021, 0.0021)
     plt.xlabel("Number of terms in the series")
     plt.ylabel("Value of π")
     plt.title("Approximation of π using the given series with λ=1 and λ=2")
@@ -50,7 +67,7 @@ def plot_values(pi_values_0, pi_values_1, pi_values_2, pi_values_3, new_limits, 
 
 
 if __name__ == "__main__":
-    new_limits = list(range(1, 20))
+    new_limits = list(range(1, 15))
 
     pi_values_0 = [calculate_pi_approx_lambda(limit, 0) for limit in new_limits]
     diff_0 = calculate_diff_from_pi(pi_values_0)
@@ -61,8 +78,8 @@ if __name__ == "__main__":
     pi_values_3 = [calculate_pi_approx_lambda(limit, 3) for limit in new_limits]
     diff_3 = calculate_diff_from_pi(pi_values_3)
 
-    file_name = "pi_values"
-    plot_values(pi_values_0, pi_values_1, pi_values_2, pi_values_3, new_limits, file_name)
+    # file_name = "pi_values"
+    # plot_values(pi_values_0, pi_values_1, pi_values_2, pi_values_3, new_limits, file_name)
 
     file_name = "diff_values"
-    plot_values(diff_0, diff_1, diff_2, diff_3, new_limits, file_name, pi_value=False)
+    plot_diffs(diff_0, diff_1, diff_2, diff_3, new_limits, file_name, pi_value=False)
