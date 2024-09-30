e8254d22e6c05c6671349d46bfc6f44e4f3acbe9
Seiya Kumada <seiya.kumada@cct-inc.co.jp>
2024-09-30 18:00:36 +0900

    例外処理を追加。

--- deep_face/myrun.sh
+++ deep_face/myrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python -m src.main \
-    --input_dir_path ./images/obama \
-    --output_dir_path ./outputs/obama \
+    --input_dir_path ./images/bush \
+    --output_dir_path ./outputs/bush \
--- deep_face/src/main.py
+++ deep_face/src/main.py
@@ -16,17 +16,33 @@ def extract_name(image_path: str) -> str:
 
 
 if __name__ == "__main__":
+    # Extract arguments
     args = extract_args()
     assert os.path.isdir(args.input_dir_path)
+
+    # Extract image paths
     image_paths = [os.path.join(args.input_dir_path, image) for image in os.listdir(args.input_dir_path)]
     image_paths.sort()
+
+    # Open the result file
     result_path = os.path.join(args.output_dir_path, "results.txt")
     with open(result_path, "w") as f:
+
+        # Iterate over all pairs of images
         for i in range(len(image_paths)):
             for j in range(i + 1, len(image_paths)):
-                result = DeepFace.verify(image_paths[i], image_paths[j])
+
+                print("# Save the result to a JSON file and a text file")
                 name_i = extract_name(image_paths[i])
                 name_j = extract_name(image_paths[j])
+
+                print(f"# Verify the similarity between the two images, {name_i} and {name_j}")
+                try:
+                    result = DeepFace.verify(image_paths[i], image_paths[j])
+                except Exception as e:
+                    print(f"Error: {e}")
+                    continue
+
                 output_path = os.path.join(args.output_dir_path, f"{name_i}_vs_{name_j}.json")
                 f.write(f"{name_i}-{name_j}: {result['verified']}\n")
                 json.dump(result, open(output_path, "w"))
