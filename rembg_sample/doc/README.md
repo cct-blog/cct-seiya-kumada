c7252ea8eb8ae2f0c8162fde65a5da5623198c30
Seiya <seiya.kumada@cct-inc.co.jp>
2025-01-07 13:37:31 +0900

    追加検証

--- rembg_sample/doc/README.md
+++ rembg_sample/doc/README.md
@@ -1,8 +1,11 @@
 # 環境構築手順
-いきなりpoetry add rembgするとこける。以下の手順で。
+いきなりpoetry add rembgをするとこける。以下の手順で。
 ```
 $> poetry add llvmlite@latest
 $> poetry add numba@latest
 $> poetry add onnxruntime
 $> poetry add rembg
 ```
+
+# 画像のフリー素材
+[ここ](https://www.pakutaso.com/20180256039post-15136.html)
--- rembg_sample/src/main.py
+++ rembg_sample/src/main.py
@@ -1,9 +1,26 @@
+import glob
+import os
+from email.mime import base
+
 from PIL import Image
 from rembg import remove
 
 if __name__ == "__main__":
     IMAGE_PATH = "/home/kumada/data/gemini_trial/images/cat.jpg"
     OUTPUT_PATH = "./output.png"
-    src_image = Image.open(IMAGE_PATH)
-    dst_image = remove(src_image)
-    dst_image.save(OUTPUT_PATH)
+
+    input_paths = glob.glob("/home/kumada/data/rembg/inputs/*")
+    output_dir_path = "/home/kumada/data/rembg/outputs"
+    if not os.path.exists(output_dir_path):
+        os.makedirs(output_dir_path)
+
+    for input_path in input_paths:
+        print(f"> input_path: {input_path}")
+        src_image = Image.open(input_path)
+        dst_image = remove(src_image)
+        basename = os.path.basename(input_path)
+        head, _ = os.path.splitext(basename)
+        output_path = os.path.join(output_dir_path, f"{head}.png")
+        print(f"> output_path: {output_path}")
+        dst_image.save(output_path)
+        print("Done.")
